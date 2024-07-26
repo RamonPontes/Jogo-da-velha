e241d9c5c892dd0fc5a6677bc7b53d3faf80b521
Ramon <rgdpontes@yahoo.com>
2024-07-25 23:42:46 -0300

    Primerio de muitos commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,94 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import styles from "../../styles/page.module.css";
+import {useState} from "react";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    let [player, setPlayer] = useState('X');
+    let [winner, setWinner] = useState('');
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    const checkWinner = () => {
+        const table = []
+        for (let i = 0; i < 9; i++) {
+            table.push(document.getElementById(i).innerText)
+        }
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+        for (let h = 0; h <= 6; h += 3) {
+            if(table[h] == table[h+1] && table[h+1] == table[h+2] && table[h] != "") {
+                setWinner(table[h])
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+                return true
+            }
+        }
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+        for (let v = 0; v < 3; v ++) {
+            if(table[v] == table[v+3] && table[v+6] == table[v+3] && table[v] != "") {
+                setWinner(table[v])
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
-}
+                return true
+            }
+        }
+
+        if(table[0] == table[4] && table[0] == table[8] && table[0] != "") {
+            setWinner(table[0])
+
+            return  true
+        }
+
+        if(table[2] == table[4] && table[4] == table[6] && table[6] != "") {
+            setWinner(table[2])
+
+            return true
+        }
+
+        //0 1 2
+        //3 4 5
+        //6 7 8
+        let filled = 0
+
+        for(let x = 0; x < 9; x ++) {
+            if(table[x] != ""){
+                filled+=1
+            }
+        }
+        if(filled == 9){
+            setWinner("Ninguém")
+            return true
+        }
+
+        return false
+    }
+
+    const gameClick = (possiton) => {
+        const element = document.getElementById(possiton);
+
+        if( element.innerText === ""  && !checkWinner() ) {
+            player === 'X' ? setPlayer("O") : setPlayer("X");
+
+            element.innerText = player;
+            checkWinner()
+        }
+    }
+
+    const gameGenerator = Array.from({ length: 9 }, (_, index) => (
+        <p className={styles.p} id={index} onClick={() => gameClick(index)}></p>
+    ));
+
+    const reset = () => {
+        for(let i = 0; i < 9; i++) {
+            const element = document.getElementById(i)
+            element.innerText = "";
+            setWinner("");
+        }
+    }
+
+    return (
+        <main className={styles.main}>
+            <div className={styles.container}>
+                {gameGenerator}
+            </div>
+            {winner && <h1 className={styles.h1}>{winner} é o VENCEDOR</h1>}
+            <input className={styles.btn} onClick={reset} type="button" value="RESET"/>
+        </main>
+    );
+};
